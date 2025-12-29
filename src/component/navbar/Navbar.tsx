2295ad7074a1209c7cb04a68e4d49c7bdb99181a
Navid Salehi <52621302+navidslh2@users.noreply.github.com>
2025-12-29 22:59:33 +0330

    test

--- src/component/navbar/Navbar.tsx
+++ src/component/navbar/Navbar.tsx
@@ -10,7 +10,7 @@ import Mobilemenu from "./Mobilemenu";
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   return (
-    <header className="fixed top-5 left-5 right-5 h-[60px]  px-5 md:px-10 flex items-center justify-between  border border-gray-800 bg-gray-700  rounded-xl shadow-[0_4px_30px_rgba(0,0,0,0.3)] z-20">
+    <header className="fixed top-5 left-5 right-5 h-[60px]  px-4 md:px-10 flex items-center justify-between  border border-gray-800 bg-gray-700  rounded-xl shadow-[0_4px_30px_rgba(0,0,0,0.3)] z-20">
       <AlignCenterIcon
         className="md:hidden text-white  hover:text-blue-400 cursor-pointer"
         onClick={() => setIsMobileMenuOpen(true)}
